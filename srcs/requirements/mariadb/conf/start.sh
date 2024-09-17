3b7ddb41eca2d79406daa8f818b08f68e3ce6b82
Victor Zayas <55438812+victor-zayas@users.noreply.github.com>
2024-09-17 11:34:38 +0000

    wordpress no me ama

--- srcs/requirements/mariadb/conf/start.sh
+++ srcs/requirements/mariadb/conf/start.sh
@@ -1,11 +1,11 @@
-chown -R mysql /var/lib/mysql
+chown -R mariadb /var/lib/mariadb
 
-sed -i "s/^bind-address\s*=.*$/#bind-address = 127.0.0.1/" /etc/mysql/mariadb.conf.d/50-server.cnf
-sed -i "s/skip-networking/#skip-networking/g" /etc/mysql/mariadb.conf.d/50-server.cnf
+sed -i "s/^bind-address\s*=.*$/#bind-address = 127.0.0.1/" /etc/mariadb/mariadb.conf.d/50-server.cnf
+sed -i "s/skip-networking/#skip-networking/g" /etc/mariadb/mariadb.conf.d/50-server.cnf
 
 # Configurar puerto en my.cnf
-sed -i "s/#\?port\s*=.*/port=3306/" /etc/mysql/mariadb.conf.d/50-server.cnf
+sed -i "s/#\?port\s*=.*/port=3306/" /etc/mariadb/mariadb.conf.d/50-server.cnf
 
-/etc/init.d/mysql restart
+/etc/init.d/mariadb restart
 chmod +x /tmp/setup.sh
-service mysql restart
+service mariadb restart
